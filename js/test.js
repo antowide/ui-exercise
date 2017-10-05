892d26775a7c2a945aaaa00ece9bc83060905d20
Anthony Owide <antowide@hotmail.com>
2017-10-05 16:15:12 +0100

    Add a toggle button to show/hide the sidebar for excercise 3.

--- js/test.js
+++ js/test.js
@@ -1,5 +1,13 @@
 $(function() {
     console.log("DOM is ready!");
-    // $(".sidebar").hide();
-    // $("main").css("grid-template-columns","auto 0%");
+
+    $(".sidebar-trigger").click(function() {
+         $(".sidebar").toggle("slow");
+         $("main").toggleClass("xxx", "slow");
+    });
+
+    $(".no-cssgrid .sidebar-trigger").click(function() {
+         $(".no-cssgrid main .content").toggleClass("xxx", "slow");
+    });
+
 });
